7f762a1d42dac4d3bee68b8f227c62599579fa30
molamk <molamk@protonmail.com>
2018-11-26 03:16:41 -0500

    algo

---
+++ tp3/src/greedy.h
@@ -0,0 +1,9 @@
+#if !defined(GREEDY_H)
+#define GREEDY_H
+
+#include "utils.h"
+#include "unordered_set"
+
+void solveGreedy(Problem &p, std::vector<int> &solution);
+
+#endif // GREEDY_H
---
+++ tp3/src/main.cpp
@@ -0,0 +1,29 @@
+#include "utils.h"
+#include "greedy.h"
+#include "recuit.h"
+
+int main()
+{
+    srand(time(NULL));
+
+    // The edges list is a symmertrical matrix => Optimize !!
+    const std::string filename = "../instances/PCT_200_50";
+    Problem p;
+    createProblem(filename, p);
+
+    std::vector<int> s;
+    solveGreedy(p, s);
+
+    std::cout << std::endl;
+    std::cout << "Value ratio:\t" << getValueRatio(p, s) << std::endl;
+    std::cout << "Duration ratio:\t" << getDurationRatio(p, s) << std::endl;
+
+    std::vector<int> recuit_s;
+    solveRecuit(p, recuit_s);
+
+    std::cout << std::endl;
+    std::cout << "Value ratio:\t" << getValueRatio(p, recuit_s) << std::endl;
+    std::cout << "Duration ratio:\t" << getDurationRatio(p, recuit_s) << std::endl;
+
+    return 0;
+}
